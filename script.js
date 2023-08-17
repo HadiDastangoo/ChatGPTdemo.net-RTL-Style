6421533213d810b00242d96cc457322ad3063be5
Hadi Dastangoo <h.dastangoo@gmail.com>
2023-08-17 20:39:22 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // main style file url to inject
-const file_url = "https://raw.githubusercontent.com/HadiDastangoo/ChatGPTdemo.net-RTL-Style/main/style.css";
+const custom_style_url = "https://raw.githubusercontent.com/HadiDastangoo/ChatGPTdemo.net-RTL-Style/main/style.css";
 
-const file_ref = document.createElement("link");
-file_ref.setAttribute("rel", "stylesheet");
-file_ref.setAttribute("type", "text/css");
-file_ref.setAttribute("href", file_url);
+function loadStyle( file_url ) {
+	const file_ref = document.createElement("link");
+	file_ref.setAttribute("rel", "stylesheet");
+	file_ref.setAttribute("type", "text/css");
+	file_ref.setAttribute("href", file_url);
+	document.getElementsByTagName("head")[0].appendChild(file_ref);
+}
 
-// inject css file
-document.getElementsByTagName("head")[0].appendChild(file_ref);
+loadStyle( custom_style_url );
