9832dd1cd28dcee86b3cb2ab3ff026806faba7c2
Hadi Dastangoo <h.dastangoo@gmail.com>
2023-08-17 20:47:24 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// main style file url to inject
-const custom_style_url = "https://raw.githubusercontent.com/HadiDastangoo/ChatGPTdemo.net-RTL-Style/main/style.css";
-
 function loadStyle( file_url ) {
 	const file_ref = document.createElement("link");
 	file_ref.setAttribute("rel", "stylesheet");
@@ -8,5 +5,8 @@ function loadStyle( file_url ) {
 	file_ref.setAttribute("href", file_url);
 	document.getElementsByTagName("head")[0].appendChild(file_ref);
 }
+
+// main style file url to inject
+const custom_style_url = "https://raw.githubusercontent.com/HadiDastangoo/ChatGPTdemo.net-RTL-Style/main/style.css";
 
 loadStyle( custom_style_url );
